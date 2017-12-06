6cae1b9d41de6f7465f94b91fd2b1ecb0a7c3798
Denis Li <denisli@mit.edu>
2017-12-05 21:28:35 -0500

    [bug-fix] Corrected so that running SimpleDb on single machine will use port 9999.

--- src/java/simpledb/SimpleDb.java
+++ src/java/simpledb/SimpleDb.java
@@ -107,11 +107,15 @@ public class SimpleDb {
         else if (args[0].equals("simple")) {
             Database.getCatalog().loadSchema("config/child/" + 9999 + "/catalog.txt");
             QueryTree qt = QueryParser.parse(null, args[1], true /*useSimpleDb*/);
+            final long startTime = System.nanoTime();
             OpIterator it = qt.getRootOp();
             it.open();
             while (it.hasNext()) {
                 System.out.println(it.next());
             }
+            final long endTime = System.nanoTime();
+            final long duration = endTime - startTime;
+            System.out.println("Query time: " + (double)duration / 1000000.0 + "ms.");
         }
         else {
             System.err.println("Unknown command: " + args[0]);
